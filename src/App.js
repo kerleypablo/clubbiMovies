50669ba6548ba8569229800c666f70a25afb8d28
kerley <kerley.kls@gmail.com>
2022-10-03 20:31:41 -0300

    feat: primeiras configuracoes e rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+import Homepage from './page/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" element={<Homepage />} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
