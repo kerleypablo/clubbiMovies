0236d4929ac5974a700a3dd942cd0b5fc076732f
kerley <kerley.kls@gmail.com>
2022-10-03 20:15:39 -0300

    feat: creado o context e Provider

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.spec.jsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension':
+      [1, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
